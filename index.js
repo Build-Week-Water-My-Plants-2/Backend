4e6236213b78eee10c55ff3047fc7ab5e1405b4a
taterntots <matthew.a.weidner@gmail.com>
2020-02-04 14:28:07 -0500

    added some code to index.js which should prevent the deployed site from sleeping and deleting the database

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const server = require('./api/server.js');
 
+var https = require("https");
+setInterval(function() {
+    https.get("https://water-my-plants-2.herokuapp.com");
+}, 1000 * 60 * 5); // every 5 minutes (300000). Keeps the database from going to sleep in herroku and deleting.
+
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
